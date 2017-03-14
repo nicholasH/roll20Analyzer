127f863c263037008964b0ea5414605ce2678692
nicholasH <nicholas.hoover@gmail.com>
2017-03-13 19:00:23 -0700

    added comments

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from HTMLParser import HTMLParser
 import sys
 from collections import Counter
 
+#parses out the html of the chat log
 class HTMLParser(HTMLParser):
     def handle_starttag(self, tag, attrs):
         #print("Start tag:", tag)
@@ -18,6 +19,7 @@ class HTMLParser(HTMLParser):
 
 path = ""
 
+#looks for the data in the data folder
 for file in os.listdir(os.path.join(sys.path[0], "data")):
     if file.endswith(".html") and file.startswith("Chat Log for"):
         path= os.path.join(sys.path[0], "data", file)
@@ -25,12 +27,13 @@ for file in os.listdir(os.path.join(sys.path[0], "data")):
 
 
 f = open(path,'r')
+#the scrubing file that has all the usefull data in it
 wf = open(os.path.join(sys.path[0],"data\\scrub.txt"),'w')
 
 parser = HTMLParser()
 #maybe make a loading bar for this
 for line in f:
-    if "textchatcontainer" in line:
+    if "textchatcontainer" in line:# This is the container that has all the rolls and chat data
         parser.feed(f.next())
         break
 
@@ -41,6 +44,13 @@ print("done with scrub")
 f = open(os.path.join(sys.path[0],"data\\scrub.txt"),'r')
 
 playerStats = dict()#PlayerId:dict() states
+'''
+roll 20 does not put the player Id when a user makes a general message
+This means that if a player make a general message it can be by anyone
+This also means associating human readable name like "krong the mighty" to the id  is annoying
+Thankfully all posts have a player picture associated to them at appears when they make a post.
+Im using that to help associate ID with names
+'''
 currentPlayer = ""
 currentPhotoId = ""
 
@@ -64,15 +74,17 @@ for line in f:
             photos.add(photo)
             stats["photos"] = photos
             playerStats[currentPlayer] = stats
+
+#makes F go back to first line.
 f.seek(0)
 
 for line in f:
-    if "data-playerid" in line:
+    if "data-playerid" in line:#this gets the player of the message that is being analyzed
         s = line.split(" ")
         playerId = s[1]
         playerId = playerId.strip()
         currentPlayer = playerId
-    if "class avatar" in line:
+    if "class avatar" in line:#this gets the avatar of the message that is being analyzed
         s= f.next()
         currentPhotoId = s.strip()
     if "class by" in line:
@@ -81,7 +93,7 @@ for line in f:
         photos = player["photos"];
         if currentPhotoId in photos:
             player["names"].add(line.strip())
-    if "diceroll" in line:
+    if "diceroll" in line:#get all the dice rolls
         if "dropped" in line:
             continue
         player = playerStats[currentPlayer]
