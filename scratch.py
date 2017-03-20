12b46dbbcaa5ac68efa70c9e55f0feb4fe19ebf0
nicholasH <nicholas.hoover@gmail.com>
2017-03-19 20:52:46 -0700

    made time selector in the scratch file

--- scratch.py
+++ scratch.py
@@ -1,48 +1,61 @@
 from bs4 import BeautifulSoup
 import re
+from bs4.element import NavigableString
+from datetime import datetime
+
+
+
+
 
 test = []
 f = open("E:\\GitProjects\\roll20Analyzer\\data\\Chat Log for Caramohn's Level.html")
 
+
 soup = BeautifulSoup(f.read(), 'html.parser')  # make soup that is parse-able by bs
+f.close()
 
 generalmatch = re.compile('message \w+')
 
 chatContent = soup.findAll("div", {"class": generalmatch})
 
-for message in chatContent:
-    if "rollresult" in message.attrs["class"]:
-        print("worked")
 
 
-print("")
+
+def getTimeRange():
+    date1String = "Aug 26 2016"
+    date1 = datetime.strptime(date1String, '%b %d %Y')
+    date2String = "OCT 29 2016"
+    date2 = datetime.strptime(date2String,'%b %d %Y' )
+
+    startMessageIndex = 0
+    startFound = False
+    endMessageIndex = len(chatContent)
+
+    for index, chat in enumerate(chatContent):
+        for ch in chat.contents:
+            if not isinstance(ch, NavigableString):
+                s = ch.attrs.get("class")
+                if not isinstance(s, type(None)):
+                    if any("tstamp" in f for f in s):
+                        chDate = datetime.strptime(ch.string,'%B %d, %Y %I:%M%p')
+                        if chDate.date() >= date1.date() and not startFound:
+                            startMessageIndex = index
+                            startFound = True
+                        if date2.date() < chDate.date():
+                            endMessageIndex = index -1
+                            return [startMessageIndex,endMessageIndex]
+
+    return [startMessageIndex,endMessageIndex]
 
 
-def diceCounter(diceFomula):
-    s = diceFomula.attrs.get("class")
-    critsuc = 0
-    critfail = 0
-    dices = []
-
-    if any("formattedformula" in t for t in s):
-        for child in diceFomula.descendants:
-            if not isinstance(child, NavigableString):
-                childClass = child.attrs["class"]
-                if any("dicegrouping" in at for at in childClass):
-                    c = child
-
-                    print()
-
-    return [critsuc, critfail, dices]
+tr =getTimeRange()
 
 
-dice = diceFomula.next_element.next_element.contents
-if len(dice) > 0:
-    dices.append(dice[1])
-    for di in dice:
-        if not isinstance(di, NavigableString):
-            d = di.attrs["class"]
-            if any("critsuccess" in t for t in d):
-                critsuc += 1
-            if any("critfail" in t for t in d):
-                critfail += 1
+c = chatContent[tr[0]:tr[1]]
+
+
+
+
+
+print()
+
