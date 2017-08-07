1ea56d8d59488b01d175b3d83feba4d2ee2dd304
Nicholas Hoover <n.hoover13@gmail.com>
2017-08-06 22:13:57 -0700

    work

--- main.py
+++ main.py
@@ -2,74 +2,24 @@ from random import randint
 
 from kivy.app import App
 from kivy.properties import NumericProperty, ReferenceListProperty, Clock, ObjectProperty
+from kivy.uix.button import Button
 from kivy.uix.widget import Widget
 from kivy.vector import Vector
 
 
-class PongGame(Widget):
-    ball = ObjectProperty(None)
-    player1 = ObjectProperty(None)
-    player2 = ObjectProperty(None)
 
-    def serve_ball(self):
-        self.ball.center = self.center
-        self.ball.velocity = Vector(4, 0).rotate(randint(0, 360))
+def callback(instance):
+    print('The button <%s> is being pressed' % instance.text)
 
-    def update(self, dt):
-        self.ball.move()
+btn1 = Button(text='Hello world 1')
+btn1.bind(on_press=callback)
+btn2 = Button(text='Hello world 2')
+btn2.bind(on_press=callback)
 
-        self.player1.bounce_ball(self.ball)
-        self.player2.bounce_ball(self.ball)
-
-        # bounce off top and bottom
-        if (self.ball.y < 0) or (self.ball.top > self.height):
-            self.ball.velocity_y *= -1
-
-        # bounce off left and right
-        if (self.ball.x < 0) or (self.ball.right > self.width):
-            self.ball.velocity_x *= -1
-
-
-        # went of to a side to score point?
-        if self.ball.x < self.x:
-            self.player2.score += 1
-            self.serve_ball(vel=(4, 0))
-        if self.ball.x > self.width:
-            self.player1.score += 1
-            self.serve_ball(vel=(-4, 0))
-
-    def on_touch_move(self, touch):
-        if touch.x < self.width / 3:
-            self.player1.center_y = touch.y
-        if touch.x > self.width - self.width / 3:
-            self.player2.center_y = touch.y
-
-
-class dndUIApp(App):
+class PongApp(App):
     def build(self):
-        game = PongGame()
-        game.serve_ball()
-        Clock.schedule_interval(game.update, 1.0 / 60.0)
-        return game
-
-class PongPaddle(Widget):
-
-    score = NumericProperty(0)
-
-    def bounce_ball(self, ball):
-        if self.collide_widget(ball):
-            speedup  = 1.1
-            offset = 0.02 * Vector(0, ball.center_y-self.center_y)
-            ball.velocity =  speedup * (offset - ball.velocity)
-
-class PongBall(Widget):
-    velocity_x = NumericProperty(0)
-    velocity_y = NumericProperty(0)
-    velocity = ReferenceListProperty(velocity_x, velocity_y)
-
-    def move(self):
-        self.pos = Vector(*self.velocity) + self.pos
+        return btn2
 
 
 if __name__ == '__main__':
-    dndUIApp().run()
+    PongApp().run()
