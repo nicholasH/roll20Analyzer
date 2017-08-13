3c2fa757bcafd6cacbae36c16a1c0fc1a3cd2b1d
Nicholas Hoover <n.hoover13@gmail.com>
2017-08-13 13:09:51 -0700

    work

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from kivy.uix import layout
 from kivy.uix.button import Button
 from kivy.uix.widget import Widget
 from kivy.uix.gridlayout import GridLayout
+from kivy.uix.boxlayout import BoxLayout
 from kivy.vector import Vector
 import analyze
 
@@ -13,14 +14,19 @@ import analyze
 def callback(instance):
     print(instance)
 
-class dndUI(Widget):
-    pass
+class dndUI(BoxLayout):
+    def __int__(self,**kwargs):
+        super(dndUI,self).__init__(**kwargs)
 
 
 
 class dndUIApp(App):
+    def __int__(self,**kwargs):
+        super(dndUIApp,self).__init__(**kwargs)
+
     def build(self):
         return dndUI()
+
 
 
 if __name__ == '__main__':
