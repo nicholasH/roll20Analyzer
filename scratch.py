8245212efb22f88edcc11b41411e7901ebd62a61
Nicholas Hoover <n.hoover13@gmail.com>
2017-10-09 17:55:37 -0700

    work

--- scratch.py
+++ scratch.py
@@ -2,12 +2,27 @@ import os
 from tkinter import filedialog
 from tkinter import *
 
-hightroll = [None, 0]
-highestCritsus = [None, 0]
-highestNats = [None, 0]
-print([highestNats[0],highestCritsus[0],hightroll[0]])
 
-if any(a is None for a in [highestNats[0],highestCritsus[0],hightroll[0]]):
-    print(" worked")
-else:
-    print("didnt")
+#start a tag
+"&"
+#the word affter the & is the tag name. A tag like this will only work the the next roll
+"&tag"
+
+#this  tag will tag every post for the next 24 hours
+'&tag -24h'
+
+#this will tag every thing in the next 60 min
+'&tag -24m'
+
+#this tag will tag all post with out end
+'&tag -start'
+
+#this is end a tag by this tag name
+'&tag -end'
+
+#this will end all tags for every one
+'&tag -endall'
+
+#this make tags only for the oneself
+'&tag -self'
+'&tag -24m -self'
